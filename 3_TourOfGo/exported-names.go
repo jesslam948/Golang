d7e3d0e9d677431b0ad6164be1e92116157f7b60
Jessica Lam <jess.lam948@gmail.com>
2021-02-17 16:51:35 -0800

    learn exported names

---
+++ 3_TourOfGo/exported-names.go
@@ -0,0 +1,15 @@
+// Description: Exploring exported names
+// Resources:
+//	https://tour.golang.org/basics/3
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("pi:", math.Pi)
+	fmt.Println("e:", math.E)
+}
